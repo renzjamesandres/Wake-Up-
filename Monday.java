b8ac7390790a42659faeb731122fe7a3e0cfdbcb
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-03 23:19:13 +0800

    Update Monday.java

--- Monday.java
+++ Monday.java
@@ -1,40 +1,48 @@
-import java.time.LocalTime;
-import java.time.format.DateTimeParseException;
 import java.util.Scanner;
+import java.time.format.DateTimeParseException;
+import java.time.LocalTime;
 
-public class Monday extends Weekday {
-    private String time; 
+// Define the Alarm interface
+interface Alarm {
+    void setAlarm(String time);
+    void showAlarm();
+}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter time for alarm in this format (HH:MM): ");
-        String timeInput = scanner.nextLine();
-
-        Monday monday = new Monday();
-        monday.setAlarm(timeInput);
-        monday.showAlarm();
-
-        scanner.close();
-    }
+// Define the abstract Weekday class that implements the Alarm interface
+abstract class Weekday implements Alarm {
+    private String alarmTime;
 
     @Override
     public void setAlarm(String time) {
-        this.time = time;
+        this.alarmTime = time;
     }
 
     @Override
     public void showAlarm() {
-        try {
-            LocalTime alarm = LocalTime.parse(time);
-            LocalTime now = LocalTime.now();
+        LocalTime alarm = LocalTime.parse(alarmTime); // Convert String to LocalTime
+        LocalTime now = LocalTime.now(); // Get the current time
 
-            if (alarm.isAfter(now)) {
-                System.out.println("I'll wake you up later!");
-            } else {
-                System.out.println("Alarm is set for tomorrow!");
-            }
-        } catch (DateTimeParseException e) {
-            System.out.println("Invalid time format. Please enter time in HH:MM format.");
+        // Check if the alarm time is before the current time
+        if (alarm.isBefore(now)) {
+            System.out.println("Alarm is set for tomorrow!");
+        } else {
+            System.out.println("I'll wake you up later!");
         }
+    }
+}
+
+// Define the public class Monday that contains the main method
+public class Monday extends Weekday {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Monday monday = new Monday();
+        
+        System.out.print("Enter time for alarm in this format (HH:MM): ");
+        String timeInput = scanner.nextLine();
+
+        monday.setAlarm(timeInput);
+        monday.showAlarm();
+        
+        scanner.close();
     }
 }
